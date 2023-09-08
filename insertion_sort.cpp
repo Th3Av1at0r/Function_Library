24927f8d0799f7c55cb2e29ac0cf02358c8f7281
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2023-09-08 16:47:07 -0500

    Update insertion_sort.cpp

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -1,1 +1,49 @@
+//
+//  main.cpp
+//  insertion_sort
+//
+//  Created by Avi Schmookler on 9/8/23.
+//
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int i = 1;
+    int j;
+    int array[20] = {23, 34, 55, 77, 2, 5, 99, 12, 41, 754, 1, 25, 80, 30, 10, 27, 76, 79, 35, 85};
+    int n = 20;
+    int transfer_variable;
+
+    
+    while (i < n)
+    {
+        j = i;
+        while (j > 0 && array[j - 1] > array [j])
+        {
+            transfer_variable = array[j];
+            array[j] = array[j - 1];
+            array[j - 1] = transfer_variable;
+            j -= 1;
+        }
+        i += 1;
+    }
+    
+    i = 1;
+    
+    while(i <= n)
+    {
+        if (i % 8 == 0)
+        {
+            std::cout << setw(10) << array[i] << "\n";
+        }
+        else
+        {
+            std::cout << setw(10) << array[i];
+        };
+        
+        i += 1;
+    }
+}
 
