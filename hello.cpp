6595b9655fa4cfec3efd0484020b3a247bae1747
Th3Av1at0r <77683463+Th3Av1at0r@users.noreply.github.com>
2023-09-08 22:12:28 -0500

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by Avi Schmookler on 9/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
